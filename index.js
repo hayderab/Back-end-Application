18115e5ad2d3b1b028ad6e3ff061a160f01fd39f
abbasih3 <abbasih3@coventry.ac.uk>
2021-03-27 00:38:17 +0000

    added routes to index file and other modules

---
+++ index.js
@@ -0,0 +1,41 @@
+const express  = require('express');
+const app =  express(); 
+const database = require("./helpers/databse.js")
+
+const users = require('./routes/users.js');
+
+const dogs = require('./routes/dogs.js');
+const shelters = require('./routes/shelters.js');
+const auth = require("./controllers/auth.js")
+
+//  const Welcome = (req, res) => {
+//     res.json("Welcome to DogShleter Api");
+//  }
+ 
+
+app.use(express.json());
+app.use('/api/users',users); 
+app.use('/api/auth',auth); 
+app.use('/api/dogs',dogs); 
+
+// app.use('/api/shelters',shelters); 
+
+
+// app.use('/api/', Welcome); 
+//  app.use(dogs.routes());
+//  app.use(shelters.routes());
+
+// app.use(users.routes()); 
+// app.use(dogs.routes());
+// app.use(shelters.routes());
+
+
+
+database();
+
+ let port = process.env.PORT || 5000;
+
+// Finally, run the app as a process on a given port
+
+app.listen(port);
+console.log(`listing on ${port}`)
