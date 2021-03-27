03b1b85cbf1dda9c2800676c9161c55b0df9a8fe
abbasih3 <abbasih3@coventry.ac.uk>
2021-03-27 00:42:37 +0000

    created mongoose data schemas create schema for user created schema for dogs created schema for shelters

---
+++ models/dogs.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const  Schema = mongoose.Schema;
+
+const DogsSchema = new Schema({
+  name: {
+    type:String, 
+    required:true},
+  type:{
+    type:String,
+    required:true}, 
+  location:{
+    type:String, 
+    required: true}, 
+  avilable:{
+    type:Boolean, 
+    required: true},  
+  dateAdded:{
+    type: Date,
+    default: Date.now
+},
+  dateUpdated:{
+    type: Date,
+    default: Date.now
+}
+
+});
+module.exports = dogs = mongoose.model('dogs', DogsSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ models/users.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type:String,
+    required:true}, 
+  lastName:{
+    type: String, 
+    required: true}, 
+  email:{
+    type: String, 
+    required:true}, 
+  sigupcode:{
+    type: String}, 
+  password:{
+    type : String,
+    required: true}, 
+  dataCreated: {
+    type: Date,
+    default: Date.now
+}
+});
+
+module.exports = users = mongoose.model('users', userSchema);
+
+
+
+
+
+
+
