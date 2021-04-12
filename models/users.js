559901212bdcc9c3b1af472934a54a3833a9786e
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-12 04:51:28 +0100

    user schema

--- models/users.js
+++ models/users.js
@@ -3,29 +3,33 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   firstName: {
-    type:String,
-    required:true}, 
-  lastName:{
-    type: String, 
-    required: true}, 
-  location:{
-   type: String, 
-   required: true
+    type: String,
+    required: true
   },
-  email:{
-    type: String, 
-    required:true}, 
-  sigupcode:{
-    type: String}, 
-  password:{
-    type : String,
-    required: true}, 
+  lastName: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  sigupcode: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: true
+  },
   dataCreated: {
     type: Date,
     default: Date.now
-}
+  }
 });
-
 module.exports = users = mongoose.model('users', userSchema);
 
 
