790cff8a1a56dc879b258a5ce982851173c15a8d
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-12 04:51:49 +0100

    accessed based on location

--- permissions/locRole.js
+++ permissions/locRole.js
@@ -12,40 +12,16 @@ const Dogs = require('../models/dogs')
 
 // checking if the user has signup code to grant further permissions.
 function locRole(req, res, next ){
-    
-
-//    Dogs.findById(req.params.id)
-//     .select("location")
-//     .then(dogs =>{
-//         // locationA = dogs.location
-//        console.log(dogs)
-//     })
-//     // if (Users.location == req.body.location ){
-//     //     console.log("goto next page");
-      
-//     //   }
-//     // console.log(req.body.location )
-
-
    Users.findById(req.user.id)
     .select("-password")
     .then(user =>{
-        // console.log(user.location)
-        // locationB = user.location
         Dogs.findById(req.params.id).then(dogs =>{
-
             if(dogs.location == user.location){
                 next(); 
             }else{
                 res.status(403).json({message:"Premission denied"})      
             }
         })
-       
-
-     
     })
-
-   
 }
-
 module.exports = locRole;
