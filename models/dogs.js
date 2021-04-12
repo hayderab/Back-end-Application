f2ca92a367718aa8f1ef2d9b58b3cd8722cea1ea
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-12 04:49:35 +0100

    added validation

--- models/dogs.js
+++ models/dogs.js
@@ -1,27 +1,31 @@
 const mongoose = require('mongoose');
-const  Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const DogsSchema = new Schema({
   name: {
-    type:String, 
-    required:true},
-  type:{
-    type:String,
-    required:true}, 
-  location:{
-    type:String, 
-    required: true}, 
-  avilable:{
-    type:Boolean, 
-    required: true},  
-  dateAdded:{
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  avilable: {
+    type: Boolean,
+    required: true
+  },
+  dateAdded: {
     type: Date,
     default: Date.now
-},
-  dateUpdated:{
+  },
+  dateUpdated: {
     type: Date,
     default: Date.now
-}
+  }
 
 });
 module.exports = dogs = mongoose.model('dogs', DogsSchema);
