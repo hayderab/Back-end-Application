56fe439fe6ad99330e189a2b5e345d8eb22ce820
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-18 16:09:51 +0100

    added userid in reponse

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,8 +11,7 @@ const { findById, findByIdAndUpdate, findByIdAndDelete } = require('../models/us
 
 const auth = require("../strategies/auth_token.js")
 
-
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // body data
   const { email, password } = req.body;
 
@@ -20,26 +19,25 @@ router.post('/', (req, res) => {
   if (!email || !password) {
     return res.status(400).json({ message: "Please enter all feilds" });
   }
-  Users.findOne({ email })
+  await Users.findOne({ email })
     .then(user => {
-      if (!user) return res.status(400).json({ message: "User dosnot exists" });
+      if (!user) return res.status(400).json({ message: "User does not exists" });
       //validating password
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (!isMatch) return res.status(400).json({ message: "invalid crendentials" })
           token = authtoken(user);
-          res.status(202)
+          res.status(200)
             .cookie('token', token, {
               sameSite: "strict",
               path: '/',
               httpOnly: true
             }).json({
+              userId: user.id,
               sigupcode: user.sigupcode,
               message: "login Successful."
             })
         })
-
-
     });
 });
 
@@ -48,66 +46,55 @@ router.get("/logout", (req, res) => {
 })
 
 
-router.post("/logedin", (req, res) => {
+
+/**
+ * Post required for checking if the user is logged in through cookie.
+ * @param {cookie} req request cookie for checking if user logged in
+ * @param {Json} res  return json wiht true and false based on if user logged in
+ */
+router.post("/logedin",async (req, res) => {
   // get the token from the cookie. 
   // console.log(role);
   const token = req.cookies.token;
-  console.log(token);
   // checking the token...
   if (!token) {
     // chekcing if the not token is there if not send false.. 
     res.status(202).json(false);
   }
-  try {
-
+  else {
     //verifying token
-    const decode = jwt.verify(token, "thesecretkey"); 
-        // add the user from payload. 
-    // add the user from payload.
-    console.log(decode.id) 
-    // console.log(role)
-
-    Users.findById(decode.id)
-    .then( user => {
-      if(user.sigupcode == "600cem"){
-        res.json({"sigupcode": true, "login":true});
-      }
-      else{
-        res.json({"sigupcode": false, "login":true});
-      }
-    })
-    // console.log("tokin decoding.....................")
-
+    const decode = jwt.verify(token, "thesecretkey");
+    // add the user from payload. 
+    await Users.findById(decode.id)
+      .then(user => {
+        if (user.sigupcode === "600cem") {
+          res.json({ "sigupcode": true, "login": true });
+        }
+        else {
+          res.json({ "sigupcode": false, "login": true });
+        }
+      }).catch(err => res.status(403).json({ message: err }))
     // res.send(true)
-  } catch (err) {
-    //if cookie not found send not logedin.
-    res.json({message:"failed"})
   }
+
 });
 
-
-
-
+/**
+ * 
+ * @param {userid} user getting user id
+ * @returns generating token and adding user id.
+ */
 function authtoken(user) {
   var token = jwt.sign(
     { id: user.id },
     "thesecretkey",
-    { expiresIn: 5000 })
+    { expiresIn: 50000 })
+    
   return token;
 }
 
 
 
-function sigupcode(id){
-
-//  const user =  Users.findById(id);
-//  if(user.sigupcode == "600cem"){
-//   return true
-
-//  }
-//  return false
-   
-}
 
 //@route Get api/auth/user
 //@desc Auth user
