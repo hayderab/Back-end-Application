270eeae6c001337169a9703b16a760c19db7965d
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-25 03:17:14 +0100

    moved all the endpoints

---
+++ app.js
@@ -0,0 +1,28 @@
+const express  = require('express');
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+
+const app =  express(); 
+
+const database = require("./helpers/databse.js")
+const users = require('./routes/users.js');
+const dogs = require('./routes/dogs.js');
+const message = require('./routes/messages.js');
+const auth = require("./controllers/auth.js")
+
+
+ 
+app.use('/uploads', express.static('uploads'))
+app.use(express.json());
+app.use(cors({origin: "http://localhost:3000", credentials: true}));
+app.use(cookieParser());
+app.use('/api/users',users); 
+app.use('/api/auth',auth); 
+app.use('/api/dogs',dogs); 
+app.use('/api/message',message); 
+
+app.use(cookieParser());
+database();
+
+module.exports = app;
