5509d3ecfb0f151bbcafc0e1a07e4c3c1da10d6d
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-25 03:20:37 +0100

    accessing url from evn

--- helpers/databse.js
+++ helpers/databse.js
@@ -4,15 +4,16 @@ const mongoose = require('mongoose');
 
 // conncting to the database.
 const uri = "mongodb+srv://600cem-cw:600cem-cw@cluster0.x3avy.mongodb.net/dogs_shelters?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://600cem-cw:600cem-cw@cluster0.x3avy.mongodb.net/test_shelters?retryWrites=true&w=majority";
 
 const dbconnection =  async () => {
-    await mongoose.connect(uri, {
+    await mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
         useCreateIndex: true
       })
-      .then(() => console.log("Database connected..."))
+      // .then(() => console.log("Database connected..." ))
       .catch(err => console.log(err));
 }
 
