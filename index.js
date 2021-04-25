a0770611e9c479ece4200db6de940a66eeb5cc98
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-25 03:17:46 +0100

    moved routs to app.js

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const express  = require('express');
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const app =  express(); 
-const database = require("./helpers/databse.js")
-const users = require('./routes/users.js');
-const dogs = require('./routes/dogs.js');
-const shelters = require('./routes/shelters.js');
-const auth = require("./controllers/auth.js")
+const app = require('./app');
+// const cors = require('cors')
+// const cookieParser = require('cookie-parser')
 
-//  const Welcome = (req, res) => {
-//     res.json("Welcome to DogShleter Api");
-//  }
+
+// const app =  express(); 
+// const database = require("./helpers/databse.js")
+// const users = require('./routes/users.js');
+// const dogs = require('./routes/dogs.js');
+// const message = require('./routes/messages.js');
+// const auth = require("./controllers/auth.js")
+
+
  
-app.use('/uploads', express.static('uploads'))
-app.use(express.json());
-app.use(cors({origin: "http://localhost:3000", credentials: true}));
-app.use(cookieParser());
-app.use('/api/users',users); 
-app.use('/api/auth',auth); 
-app.use('/api/dogs',dogs); 
+// app.use('/uploads', express.static('uploads'))
+// app.use(express.json());
+// app.use(cors({origin: "http://localhost:3000", credentials: true}));
+// app.use(cookieParser());
+// app.use('/api/users',users); 
+// app.use('/api/auth',auth); 
+// app.use('/api/dogs',dogs); 
+// app.use('/api/message',message); 
 
 
-// app.use('/api/shelters',shelters); 
-app.use(cookieParser());
-database();
-
+// app.use(cookieParser());
+// database();
  let port = process.env.PORT || 5000;
 
 // Finally, run the app as a process on a given port
-
 app.listen(port);
 console.log(`listing on ${port}`)
